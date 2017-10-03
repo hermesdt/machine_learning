80a5c0238e4c360ab31cb56dd81aba5ac1d395e6
Álvaro Durán Tovar <hermesdt@gmail.com>
2017-10-03 20:26:54 +0100

    adds code to svm

--- svm/svm.jl
+++ svm/svm.jl
@@ -1,45 +1,74 @@
-"""
-Implementation of SVM using SMO
-"""
+include("../mnist.jl")
+include("../utils.jl")
 
-X = zeros(1000, 734)
-a = zeros(1000, 1)
-y = zeros(1000, 1)
+MAX_TRAIN = 100
+MAX_TEST = 100
 
-H(x, w, b) = sign(w' * X' + b) # should convert to int?
-error(x, y, w, b) = H(x, w, b) - y
+train_images, train_labels = MNIST.train.images(;center=true)[1:MAX_TRAIN,:], MNIST.train.labels()[1:MAX_TRAIN,:]
+test_images, test_labels = MNIST.test.images(;center=true)[1:MAX_TEST,:], MNIST.test.labels()[1:MAX_TEST,:]
 
-C = ??
-ϵ = ??
+m, n = size(train_images)
+X = train_images
+A = zeros(m, 1)
+w = zeros(n, 1)
+Y = zeros(m, 1)
+b = 0
 
-Kernel(x1, x2) = x1*x2' # linear
+Hypotesis(x, w, b) = sign.(w'*x' + b) # should convert to int?
+Hypotesis(X[1:1,:], w, b)
+ERROR(x, y, w, b) = Hypotesis(x, w, b) - y
+ERROR(X[1:1,:], Y[1:1,:], w, b)
+C = 1
+ϵ = 2
 
-while true
-    given {
-        i, j
-        a1, a2
-        x1, x2
-        y1, y2
-    }
+KERNEL(x1, x2) = x1*x2' # linear
 
-    w = sum(a*y*x)
-    b = mean(y - w*x)
+function get_rand_int(a, b, i)
+    temp = i
+    while temp == i
+        temp = rand(a:b)
+    end
+    temp
+end
 
+X = train_images
+Y = train_labels
 
-    K_x1_x2 = Kernel(x1, x1) + Kernel(x2, x2) -2Kernel(x1, x2)
+Y = Utils.convert_y_to_binary(train_labels, 5)
+Y[Y .== 0] = -1
 
-    E2, E1 = error(x2, y2, w, b), error(x1, y1, w, b)
-    a2_new = a2 + y2(E2 - E1)/K_x1_x2
+for i in 1:m
+    # i = 1
+    j = get_rand_int(1, m, i)[1]
 
-    if y_i != y_j
-        L, H = (maximum(0, a2 - a1), minimum(C, C - a1 + a2))
+    a1, a2, y1, y2 = A[i], A[j], Y[i], Y[j]
+    x1, x2 = X[i:i, :], X[j:j, :]
+
+    w = X' * (A.*Y)
+    b = mean(Y - X*w)
+
+    K_x1_x2 = KERNEL(x1, x1) + KERNEL(x2, x2) -2*KERNEL(x1, x2)
+
+    E2, E1 = ERROR(x2, y2, w, b), ERROR(x1, y1, w, b)
+    a2_new = a2 + y2*(E2 - E1)/K_x1_x2
+
+    if y1 != y2
+        L, H = (maximum([0.0, a2 - a1]), minimum([C, C - a1 + a2]))
     else
-        L, H = (maximum(0, a1 + a2 - C), minimum(C, a1 + a2))
+        L, H = (maximum([0.0, a1 + a2 - C]), minimum([C, a1 + a2]))
     end
 
-    a2_new = maximum(a2_new, L) |> minimum(H)
+    a2_new = maximum([a2_new[1], L])
+    a2_new = minimum([a2_new, H])
 
-
+    println(y1, y2)
     s = y1*y2
-    a1_new + s*a2_new = a1 + s*a2
+    a1_new = a1 + s*a2 - s*a2_new
+
+    println("a1_new $a1_new, a2_new $a2_new, b $b")
+
+    A[i] = a1_new[1]
+    A[j] = a2_new[1]
 end
+
+w
